cba779be38b450b2ad81109138ab020c16202398
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2020-05-11 00:07:53 -0300

    Update Script2.js

--- Script2.js
+++ Script2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Voxed - Autocargar nuevos comentarios al clickear en Comentar
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @include      /.*www.voxed.net/[a-z]{3}/.*\
