53d50a6ec0686abd623f033ff1e84b68c887773f
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2020-05-11 00:04:01 -0300

    Update Script3.js

--- Script3.js
+++ Script3.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @include      /.*www.voxed.net/[a-z]{3}/[a-z].*\
+// @include      /.*www.voxed.net/[a-z]{3}/.*\
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @updateURL    https://raw.githubusercontent.com/randomperson190/VoxedExtensiones/master/Script3.js
