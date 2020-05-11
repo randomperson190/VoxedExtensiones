fc0ca76a3d57b681816359700556f4415457f4c0
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2020-05-10 23:54:07 -0300

    Update Script4.js

--- Script4.js
+++ Script4.js
@@ -1,1 +1,19 @@
+// ==UserScript==
+// @name         Voxed - Control+Enter enviar comentario
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      /.*www.voxed.net/[a-z]{3}/[a-z].*\
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/randomperson190/VoxedExtensiones/master/Script4.js
+// @downloadURL  https://raw.githubusercontent.com/randomperson190/VoxedExtensiones/master/Script4.js
+// ==/UserScript==
+
+document.onkeyup = function (e) {
+  // ### Control + Enter ### - https://keycode.info/
+  if (e.ctrlKey && e.which == 13) {
+    document.querySelectorAll(".buttonPress.newComment")[0].click();
+  }
+};
 
