fdbcc79ae7e6d4a3ece1423605ef58bb9555053d
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2020-05-10 23:55:32 -0300

    Update Script5.js

--- Script5.js
+++ Script5.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Voxed - Ir a Comentario Primero / Último
+// @name         Voxed - Ir a Comentario [Primero / Último]
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
