873d82cf283e3ccab946713afc6f3e3e58b9ac94
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2020-05-11 00:07:59 -0300

    Update Script3.js

--- Script3.js
+++ Script3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Voxed - Control+Q escribir Flechita (>)
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @include      /.*www.voxed.net/[a-z]{3}/.*\
