1926bbec4b3c57b4c862a5cb11cf2bc35578a406
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2020-05-11 00:24:15 -0300

    Update Script2.js

--- Script2.js
+++ Script2.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @include      /.*voxed.net/[a-z]{3}/.*\
+// @include      /.*voxed.net/.*/.*\
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/randomperson190/VoxedExtensiones/master/Script2.js
 // @downloadURL  https://raw.githubusercontent.com/randomperson190/VoxedExtensiones/master/Script2.js
